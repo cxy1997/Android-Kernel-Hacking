897cda6927d4ff3ef07387536bf03f4e998e31c5
MasterofDarkArts <1023764859@qq.com>
2017-05-19 16:27:30 +0800

    Revised

--- Project 1/scratch/hello/jni/hello.c
+++
@@ -1,6 +0,0 @@
-#include "hello.h"
-int main(int argc, char *argv[])
-{
-    printf("Hello World!\n");
-    return 0;
-}
--- Project 1/scratch/hello/jni/hello.h
+++
@@ -1,4 +0,0 @@
-#ifndef HELLOHEADER_H_
-#define HELLOHEADER_H_
-#include <stdio.h>
-#endif /*HELLOHEADER_H_*/
